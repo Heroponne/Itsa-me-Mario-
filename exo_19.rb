5e3f71d8e25883f958ba6b1d8e28ef1598de77c0
Heroponne <florence.orgeret@gmail.com>
2019-10-01 21:29:09 +0200

    Modif exo_ 19.rb

--- exo_19.rb
+++ exo_19.rb
@@ -1,12 +1,10 @@
-number = 1
+number = 0
 
 my_tab = Array(50)
 50.times do |i|
 	number += 1
 	my_tab << "jean.dupont#{number}@email.com"
 	if number % 2 == 0
-	puts my_tab[i]
+	puts my_tab[i + 1]
 	end
 end
-
-puts my_tab
